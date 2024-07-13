0e8df8aa69939e6a3e6b04aa6f68f5060fb80b48
Fábio Correia <fabioqcorreia@gmail.com>
2024-07-12 21:32:06 -0300

    Inclusão de configuração básica de subredes, regra de firewall e service account

--- main.tf
+++ main.tf
@@ -1,17 +1,100 @@
 provider "google" {
-  project     = "study-251901"
-  region      = "us-central1"
-  zone        = "us-central1-b"
+  project = "study-251901"
+  region  = "us-central1"
+  zone    = "us-central1-b"
 }
 
-resource "google_compute_address" "ip_address" {
-  name = "priv-ip"
+# IAM
+
+## Service accounts
+
+resource "google_service_account" "compute_admin_service_account" {
+  account_id   = "compute-service-account"
+  display_name = "Compute Engine Service Account"
 }
+
+resource "google_project_iam_member" "sa_user_account_iam" {
+  project = "study-251901"
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${google_service_account.compute_admin_service_account.email}"
+}
+
+resource "google_project_iam_member" "admin_account_iam" {
+  project = "study-251901"
+  role    = "roles/compute.instanceAdmin.v1"
+  member  = "serviceAccount:${google_service_account.compute_admin_service_account.email}"
+}
+
+
+
+## Granular service account
+# 
+# Details on how to proceed: https://cloud.google.com/compute/docs/access/iam?hl=pt-br#compute.instanceAdmin
+#
+# resource "google_project_iam_custom_role" "basic_custom_role" {
+#   role_id     = "computeBasicCustomRole"
+#   title       = "Google Cloud Compute Basic Custom Role"
+#   description = "Papel criado para ter controle básico dos serviços google cloud"
+#   permissions = [
+#     "compute.addresses.create", 
+#     "iam.roles.create", 
+#     "iam.roles.delete"
+#   ]
+# }
+
+# Networks
+
+## VPC
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = false
+}
+
+## Subnets
+
+resource "google_compute_subnetwork" "subnet-a" {
+  name          = "subnet-a"
+  ip_cidr_range = "10.0.1.0/24"
+  network       = google_compute_network.vpc_network.id
+}
+
+resource "google_compute_subnetwork" "subnet-b" {
+  name          = "subnet-b"
+  region        = "us-east1"
+  ip_cidr_range = "10.0.2.0/24"
+  network       = google_compute_network.vpc_network.id
+}
+
+## Public IP
+
+resource "google_compute_address" "ip_address" {
+  name = "public-ip"
+}
+
+## Firewall
+
+resource "google_compute_firewall" "rules" {
+  name        = "allow-apache"
+  network     = google_compute_network.vpc_network.id
+  description = "Allow Apache 2 server from ingress"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["allow-apache"]
+
+}
+
 
 resource "google_compute_instance" "vm_a" {
   name         = "vm-a"
   machine_type = "e2-standard-2"
-  zone        = "us-central1-a"
+  zone         = "us-central1-a"
+
+  tags = ["allow-apache"]
 
   boot_disk {
     initialize_params {
@@ -20,11 +103,20 @@ resource "google_compute_instance" "vm_a" {
   }
 
   network_interface {
-    network = "default"
-
+    subnetwork = google_compute_subnetwork.subnet-a.name
+    network = google_compute_network.vpc_network.id
     access_config {
-        nat_ip = google_compute_address.ip_address.address
+      nat_ip = google_compute_address.ip_address.address
     }
   }
 
+  metadata_startup_script = "apt update && apt install apache2 -y"
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.compute_admin_service_account.email
+    scopes = ["cloud-platform"]
+  }
+
 }
+
